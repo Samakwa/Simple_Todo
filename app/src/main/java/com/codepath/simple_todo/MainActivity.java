a7c45298776599fb7130ba524d9e84b8d9b3133e
Samakwa <sampson_akwa@yahoo.co.uk>
2019-01-22 19:56:33 -0600

    Not yet completed

--- app/src/main/java/com/codepath/simple_todo/MainActivity.java
+++ app/src/main/java/com/codepath/simple_todo/MainActivity.java
@@ -2,8 +2,11 @@ package com.codepath.simple_todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -32,9 +35,21 @@ public class MainActivity extends AppCompatActivity {
         String itemText =etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText ("");
+        Toast.makeText(getApplicationContext(), "Item Added on List", Toast.LENGTH_SHORT).show();
 
 
 
+
+    }
+    private void setupListViewListerner(){
+        LvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
 
     }
 }
